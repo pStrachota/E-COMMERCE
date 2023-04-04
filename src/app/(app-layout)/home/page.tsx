993741deab402b33f602948d0bf95358d3421326
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-04-04 23:36:00 +0200

    add individual product info page

--- src/app/(app-layout)/home/page.tsx
+++ src/app/(app-layout)/home/page.tsx
@@ -2,26 +2,37 @@ import SectionTitle from '@/common/SectionTitle';
 import PageTitle from '@/common/PageTitle';
 import SearchResults from '@/search/SearchResults';
 import ProductFilter from '@/search/ProductFilter';
-import { getMetadata } from '@/seo/SeoUtils';
+import {getMetadata} from '@/seo/SeoUtils';
+import SelectedFilters from "@/search/SelectedFilters";
+import FilterDrawer from "@/search/FilterDrawer";
 
-export const metadata = getMetadata({ title: 'Search Products' });
+export const metadata = getMetadata({title: 'Search Products'});
 
 export default function SearchPage() {
-  const productFilter = <ProductFilter />;
+    const productFilter = <ProductFilter/>;
 
-  return (
-    <>
-      <PageTitle title="Search Products" srOnly />
-      <div className="grid md:grid-cols-[theme(spacing.72)_1fr] gap-2">
-        <section>
-          <SectionTitle as="h2" srOnly>
-            Search Results
-          </SectionTitle>
-          <div className="flex flex-col gap-2">
-            <SearchResults />
-          </div>
-        </section>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <PageTitle title="Search Products" srOnly/>
+            <div className="grid md:grid-cols-[theme(spacing.72)_1fr] gap-2">
+                <section className="hidden md:block px-2 max-h-[80vh] overflow-auto sticky top-24">
+                    <SectionTitle as="h2" srOnly>
+                        Filter
+                    </SectionTitle>
+                    {productFilter}
+                </section>
+                <section>
+                    <SectionTitle as="h2" srOnly>
+                        Search Results
+                    </SectionTitle>
+
+                    <div className="flex flex-col gap-2">
+                        <SelectedFilters/>
+                        <FilterDrawer>{productFilter}</FilterDrawer>
+                        <SearchResults/>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
 }
